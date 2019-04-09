7be2e451faf8574dc5da8d4577f7094542bd7229
Slakky <clydon96@gmail.com>
2019-04-09 17:42:41 +0200

    mapper

--- A1/code/mapper.py
+++ A1/code/mapper.py
@@ -1,1 +1,20 @@
 import json
+import sys
+
+
+#[f(x) if condition else g(x) for x in sequence]
+#[pair for pair in enumerate(mylist)]
+#[f(x) for x in sequence if condition]
+
+tuits = [(counter,line) for counter,line in enumerate(sys.stdin)\
+        if not line == "\n"]
+for i in range(len(tuits)):
+    tuit = json.loads(tuits[i][1])
+    print(tuit)
+#for counter, line in enumerate(sys.stdin.strip()):
+#    print(counter, line)
+#    if not line == "\n":
+#        tuits = json.loads(line)
+#    else:
+#        pass
+#print(counter)
